c75cda77df33e20529a765950908006c306ea1d7
Tam Nguyen <tamntrung@gmail.com>
2023-10-14 23:02:00 -0700

    Completed Footer and fixed responsiveness

--- my-app/src/Components/Carousel.js
+++ my-app/src/Components/Carousel.js
@@ -5,8 +5,9 @@ export default function Carousel() {
         <div className='relative flex w-full'>
             <img src={householdItems} className='w-full hero object-cover'></img>
             <div className='absolute text-wrap top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-center'>
-                <h1 className='text-3xl text-white titles md:text-6xl my-5'>Household Republic</h1>
-                <h2 className='text-white font- text md:text-3xl'>Industrial-grade & exam-grade nitrile, vinyl, and latex gloves designed for a variety of applications.</h2>
+                <h1 className='text-lg text-white titles md:text-4xl my-5'>Household Republic</h1>
+                <h2 className='text-white text-xs md:text-2xl text'>Industrial-grade & exam-grade nitrile, vinyl, and latex gloves designed for a variety of applications.</h2>
+                <button className='bg-blue-950 text-white rounded-md w-14 h-8 md:w-24 md:h-10 mt-5 text-sm md:text-md'>View</button>
             </div>
         </div>
     )
--- my-app/src/Components/Header.js
+++ my-app/src/Components/Header.js
@@ -9,11 +9,11 @@ export default function Header() {
     const scrollDirection = useScrollDirection();
 
     return (
-        <header className={`flex items-center justify-center p-4 border-solid border-b-2 sticky w-full bg-white h-24 z-50 ${scrollDirection === "down" ? "-top-24": "top-0"} transition-all duration-500`}>
-            <div className="basis-1/3 header-menu md:hidden"><FontAwesomeIcon icon={faBars} size='2xl'/></div>
-            <h1 className="w-full header-logo flex justify-center md:basis-1/3">
-                <a href='/' className="w-56 md:w-80">
-                    <img src={logo}></img>
+        <header className={`flex items-center justify-center p-4 border-solid border-b-2 sticky bg-white h-24 z-50 ${scrollDirection === "down" ? "-top-24": "top-0"} transition-all duration-500`}>
+            <div className="basis-1/4 header-menu md:hidden"><FontAwesomeIcon icon={faBars} size='2xl'/></div>
+            <h1 className="basis-2/4 header-logo flex justify-center md:basis-1/3">
+                <a href='/'>
+                    <img src={logo} className="h-16"></img>
                 </a>
             </h1>
             <nav className="hidden md:flex md:basis-1/3">
@@ -23,9 +23,9 @@ export default function Header() {
                     <li className="mx-2"><a>Contact</a></li>
                 </ul>
             </nav>
-            <div className="header-icons flex justify-end md:basis-1/3">
-                <FontAwesomeIcon icon={faUser} size="lg" className="mx-5"/>
-                <FontAwesomeIcon icon={faCartShopping} size="lg" className="mx-5"/>
+            <div className="basis-1/4 header-icons flex justify-end md:basis-1/3">
+                <FontAwesomeIcon icon={faUser} size="lg" className="basis-1/2 md:basis-1/4"/>
+                <FontAwesomeIcon icon={faCartShopping} size="lg" className="basis-1/2 md:basis-1/4"/>
             </div>
         </header>
     )
